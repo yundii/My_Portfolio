60ec5a3af5df15f80e4f8dba4e77450f0ac5c78a
yundii <yunditao143@gmail.com>
2025-03-16 21:45:46 -0700

    write Simple form submission handler

--- script.js
+++ script.js
@@ -2,5 +2,4 @@
 document.getElementById('contact-form').addEventListener('submit', function (e) {
     e.preventDefault();
     alert('Thank you for your message! I will get back to you soon.');
-    // You can add AJAX or backend integration here
   });
