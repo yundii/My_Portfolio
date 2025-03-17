baec41732ec352c6d27e2eeff743afc8c22d1a9f
yundii <yunditao143@gmail.com>
2025-03-16 21:55:05 -0700

    Update information

--- script.js
+++
@@ -1,5 +0,0 @@
-// Simple form submission handler
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for your message! I will get back to you soon.');
-  });
