27504cadd000bb024d782ba520cd1467c0db53cf
yundii <yunditao143@gmail.com>
2025-04-04 17:47:09 -0700

    Deploy the website

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
   <img src="https://img.icons8.com/color/48/000000/magritte.png"/>
 </p>
 <h1 align="center">
-  yundii_tz.com - v1
+  yundiitao.netlify.app - v1
 </h1>
 <p align="center">
-  The first iteration of <a href="https://yundii_tz.com" target="_blank">yundii_tz.com</a> built with React.js leveraging Material UI.
+  The first iteration of <a href="https://yundiitao.netlify.app" target="_blank">yundiitao.netlify.app</a> built with React.js leveraging Material UI.
 </p>
 
 <img width="1306" alt="Screen Shot 2021-03-15 at 8 29 18 PM" src="https://user-images.githubusercontent.com/32211479/111238274-24f8b100-85cd-11eb-9d5a-00f07cbc05e9.png">
--- src/components/SidebarNav.js
+++ src/components/SidebarNav.js
@@ -56,13 +56,13 @@ class SidebarNav extends React.Component {
           </Sidenav>
         )}
         <div className="sidebar-logos" href="/">
-          <a href="mailto:gazijarin@gmail.com">
+          <a href="mailto:tao.yun@northeastern.edu">
             <EmailRoundedIcon style={{ fontSize: 20 }}></EmailRoundedIcon>
           </a>
-          <a href="https://github.com/gazijarin">
+          <a href="https://github.com/yundii">
             <GitHubIcon style={{ fontSize: 19 }}></GitHubIcon>
           </a>
-          <a href="https://www.linkedin.com/in/gazi-jarin-3644b0172/">
+          <a href="https://www.linkedin.com/in/yunditao5732a/">
             <LinkedInIcon style={{ fontSize: 21 }}></LinkedInIcon>
           </a>
         </div>
